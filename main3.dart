6c756fb1dca4bb095aa08ebe42f30cefcc2e85d8
Muhammad Hashim Javed <53869097+MHashimJaved@users.noreply.github.com>
2023-03-07 05:03:35 +0500

    Create main3.dart
    Another example take any input data as int or double the data ouptut will come error free:

---
+++ main3.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+
+void main() {
+  var data = stdin.readLineSync();
+
+  print(data);
+}
+/// Another example take any input data as int or double the data ouptut will come error free
